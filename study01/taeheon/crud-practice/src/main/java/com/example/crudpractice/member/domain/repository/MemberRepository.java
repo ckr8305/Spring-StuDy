cfe17e1381891ddf0904423d86e8c0e913157857
KTH1007 <rlaxogjs2684@gmail.com>
2024-10-29 17:10:49 +0900

    :poop: Fix: JPA 기본 제공 코드 제거

--- study01/taeheon/crud-practice/src/main/java/com/example/crudpractice/member/domain/repository/MemberRepository.java
+++ study01/taeheon/crud-practice/src/main/java/com/example/crudpractice/member/domain/repository/MemberRepository.java
@@ -3,8 +3,5 @@ package com.example.crudpractice.member.domain.repository;
 import com.example.crudpractice.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findById(Long id);
 }

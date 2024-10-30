b4d72761a61aa34667bdeab6a2f297b08ca07a20
KTH1007 <rlaxogjs2684@gmail.com>
2024-10-30 15:18:03 +0900

    :sparkles: Feat: 주문 API 개발

---
+++ study01/taeheon/crud-practice/src/main/java/com/example/crudpractice/order/domain/repository/OrderProductRepository.java
@@ -0,0 +1,16 @@
+package com.example.crudpractice.order.domain.repository;
+
+import com.example.crudpractice.member.domain.Member;
+import com.example.crudpractice.order.domain.OrderProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+
+    @Query(value = "select op " +
+            "from OrderProduct  op join fetch op.order o " +
+            "where op.order.member = :member") // :member는 메서드 파라미터로 전달된 Member 객체를 바인딩
+    List<OrderProduct> findAllByMember(Member member);
+}

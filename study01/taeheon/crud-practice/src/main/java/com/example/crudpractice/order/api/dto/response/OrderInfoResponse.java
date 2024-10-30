f299d78bcf5aea87e736e334d57c909b53e09408
KTH1007 <rlaxogjs2684@gmail.com>
2024-10-30 15:20:14 +0900

    :sparkles: Feat: 주문 API 개발

---
+++ study01/taeheon/crud-practice/src/main/java/com/example/crudpractice/order/api/dto/response/OrderInfoResponse.java
@@ -0,0 +1,13 @@
+package com.example.crudpractice.order.api.dto.response;
+
+import jakarta.validation.constraints.NotNull;
+
+public record OrderInfoResponse(
+        @NotNull
+        Long MemberId,
+        @NotNull
+        Long OrderId,
+        @NotNull
+        OrderProductResponse orderProducts
+) {
+}
